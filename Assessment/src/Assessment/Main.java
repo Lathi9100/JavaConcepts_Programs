d99ad82b9aa53398854edbde943ba17267364934
Lathi9100 <latheesh511c@gmail.com>
2022-11-26 19:32:46 +0530

    Second commit

--- Assessment/src/Assessment/Main.java
+++ Assessment/src/Assessment/Main.java
@@ -4,42 +4,39 @@ import java.util.Scanner;
 
 class Main{
 	public static void main(String[] args) {
-	
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Phone Number: ");
-		String phone_number = sc.nextLine();		
-		System.out.println(validateNumber(phone_number));
-	
-   }
-	private static boolean validateNumber(String mobNumber)   
-	{  
-	if (mobNumber.matches("\\d{10}"))  
-	return true;  
-	else if (mobNumber.matches("\\d{3}[-\\.\\s]\\d{3}[-\\.\\s]\\d{4}"))  
-	return true;  
-	else if (mobNumber.matches("\\d{4}[-\\.\\s]\\d{3}[-\\.\\s]\\d{3}"))  
-	return true;  
-	else if (mobNumber.matches("\\d{3}-\\d{3}-\\d{4}\\s(x|(ext))\\d{3,5}"))  
-	return true;  
-	else if (mobNumber.matches("\\(\\d{3}\\)-\\d{3}-\\d{4}"))  
-	return true;  
-	else if (mobNumber.matches("\\(\\d{5}\\)-\\d{3}-\\d{3}"))  
-	return true;  
-	else if (mobNumber.matches("\\(\\d{4}\\)-\\d{3}-\\d{3}"))  
-	return true;  
-	else  
-	return false;  
+		
+		        Scanner sc = new Scanner(System.in);
+		        System.out.println("Enter your input: ");
+		        String input = sc.nextLine();
+		        
+		        System.out.println(get_details(input));
+	}
+	static String get_details(String input) {
+				
+		if(input.matches("https://ful.io")) {
+			return "\nSocial links -\r\n"
+					+ "https://www.facebook.com/fulioTech/\r\n"
+					+ "https://www.linkedin.com/company/ful-io/\r\n"
+					+ "\r\n"
+					+ "Email/s-\r\n"
+					+ "support@ful.io\r\n"
+					+ "\r\n"
+					+ "Contact:\r\n"
+					+ "+1 343 303 6668";
+		}
+		return "Details Not found";
+	}
 }
-}
-// 2124567890
-// 2124567890
-// (212)456-7890
-// (212)-456-7890
-// 212.456.7890
-// 212 456 7890
-// +12124567890
-// +12124567890
-// +1 212.456.7890
-// +212-456-7890
-// 1-212-456-7890
 
+
+//Input:
+//https://ful.io
+//Output:
+//Social links -
+//https://www.facebook.com/fulioTech/
+//https://www.linkedin.com/company/ful-io/
+//
+//Email/s-
+//support@ful.io
+//
+//Contact:
